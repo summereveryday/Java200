c4198bd794e46b825b8556df80006b2657e06d6f
Summer Daily <dailycreativemedia@gmail.com>
2019-09-11 18:34:37 -0400

    HW 2.3

--- Mod1HW/src/Main.java
+++ Mod1HW/src/Main.java
@@ -1,2 +1,24 @@
+import java.util.*;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("HW 1.3:");
+
+        System.out.println("HW 1.5:");
+
+        System.out.println("HW 2.3: Convert Feet into Meters");
+        double feet;
+        double meters;
+
+        System.out.print("Enter a number: ");
+        feet = in.nextDouble();
+        meters = feet / 3.281;
+        System.out.println(feet + " feet is equal to " + meters + " meters.");
+
+
+        System.out.println("HW 2.13:");
+    }
 }
+
