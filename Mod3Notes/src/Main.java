b2eb10d16ea01fb1b32480f3c55dcb9ada903b7a
Summer Daily <dailycreativemedia@gmail.com>
2019-09-20 12:55:54 -0400

    ADD: notes on methods

--- Mod3Notes/src/Main.java
+++ Mod3Notes/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         Scanner in = new Scanner(System.in);
 
         //loops: for, while, do-while
-
+        
         for (int i = 0; i <10; i++){
             System.out.print(i + "\n");
         }
@@ -23,5 +23,33 @@ public class Main {
             System.out.print("Enter the numeral 5: ");
             n = in.nextInt();
         } while (n != 5);
+
+        //METHODS
+
+        System.out.println(add(5, 3));
+        subtract(5, 3);
     }
+
+
+    // METHODS
+    // public vs private
+    // static void means there's no return
+    // main is the name of the method
+
+    public static int add(int num1, int num2){
+        int sum = num1 + num2;
+        return sum;
+    }
+
+    //overload method so now you add either ints or floats
+    public static float add(float num1, float num2){
+        float sum = num1 + num2;
+        return sum;
+    }
+
+    public static void subtract(int num1, int num2){
+        int diff = num1 - num2;
+        System.out.println(diff);
+    }
+
 }
