26fd55f95ad4d4ef1830d34329198fe87df3ac50
Summer Daily <dailycreativemedia@gmail.com>
2019-09-20 08:55:14 -0400

    ADD: while, for, do-while loops

--- Mod3Notes/src/Main.java
+++ Mod3Notes/src/Main.java
@@ -1,2 +1,27 @@
+import java.util.*;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        //loops: for, while, do-while
+
+        for (int i = 0; i <10; i++){
+            System.out.print(i + "\n");
+        }
+
+        int n;
+        System.out.print("Enter the numeral 3: ");
+        n = in.nextInt();
+        while (n != 3){
+            System.out.print("C'mon. That's not 3. Try again: ");
+            n = in.nextInt();
+        }
+
+
+        do {
+            System.out.print("Enter the numeral 5: ");
+            n = in.nextInt();
+        } while (n != 5);
+    }
 }
